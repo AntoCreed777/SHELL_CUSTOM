c1c7cf1835fa40fc1a4e63a0eaa2d9a218029c76
AntoCreed777 <abenavides@desem.cl>
2024-09-06 23:01:40 -0400

    feat: Implementacion de ejecutar comandos en RAM de 'favs'

--- src/favs.c
+++ src/favs.c
@@ -197,59 +197,19 @@ void buscar_favs(char *busqueda){
 }
 
 void ejecutar_favs(int numero){
-    // En caso de que atoi arroje un numero menor a 0
-    if(numero <= 0){
-        perror(ROJO "Número invalido." RESET_COLOR);
+    int tam_cache = 0;
+
+    if (cache_comandos != NULL)
+        for(int i=0;cache_comandos[i] != NULL;i++) 
+            tam_cache++;
+
+    if(numero <= 0 || numero > tam_cache){
+        printf(ROJO "Numero invalido\n" RESET_COLOR);
         return;
     }
 
-    FILE *origenFile = fopen(archivo_favs, "r");
-
-    if (origenFile == NULL) {
-        perror(ROJO "No se pudo abrir el archivo de origen" RESET_COLOR);
-        return;
-    }
-
-    char buffer[BUFFER_SIZE];
-    int contador = 1;
-    // Buscamos la posicion de el comando
-    while(fgets(buffer, sizeof(buffer), origenFile) != NULL){
-        if(contador++ == numero){
-            // Quitamos valores sobrantes e innecesarios
-            buffer[strcspn(buffer, ";")] = 0;
-            buffer[strcspn(buffer, "\n")] = '\0';
-
-            char **comando_ejecutar = NULL;
-            int tamaño = 0;
-
-            char *token = strtok(buffer, " ");
-            // Creamos el array de comandos para poder pasarlo a manejo de comandos
-            while (token != NULL) {
-                comando_ejecutar = (char **)realloc(comando_ejecutar, sizeof(char *) * ++tamaño);
-                if (comando_ejecutar == NULL) {
-                    perror(ROJO "Error en la reasignación de memoria" RESET_COLOR);
-                    liberar_comandos();
-                    liberar_cache();
-                    exit(EXIT_FAILURE);
-                }
-                comando_ejecutar[tamaño - 1] = strdup(token);
-                token = strtok(NULL, " ");
-            }
-            // Ultimo comando lo dejamos en NULl
-            comando_ejecutar = (char **)realloc(comando_ejecutar, sizeof(char *) * (tamaño + 1));
-            comando_ejecutar[tamaño] = NULL;
-
-            if(manejar_comandos_internos(comando_ejecutar)) continue;
-            manejar_comandos_externos(comando_ejecutar);
-
-            // Liberar memoria xd
-            for (int i = 0; i < tamaño; i++) {
-                free(comando_ejecutar[i]);
-            }
-            free(comando_ejecutar);
-        }
-    }
-    fclose(origenFile);
+    if(manejar_comandos_internos(cache_comandos[numero-1])) return;
+    manejar_comandos_externos(cache_comandos[numero-1]);
 }
 
 void guardar_ruta_favs(){
